25ca568966a7b961be03bd126388018aea5ca4f8
Roberto Simões <robertojuniorn@gmail.com>
2023-10-26 12:40:17 +0200

    new

--- js/script.js
+++ js/script.js
@@ -2,6 +2,8 @@
 
 const key = "7bf1eb792d7636a35af73cd8a864d47f"
 
+// This function uses the DOM to access and manipulate the HTML tag
+
 function setHtml (data) {
     console.log(data)
     document.querySelector('.city').innerHTML = data.name
@@ -11,12 +13,14 @@ function setHtml (data) {
     document.querySelector('.icon').src = `https://openweathermap.org/img/wn/${data.weather[0].icon}.png`
 }
 
+// This function get api data using async await
 
 async function getCity(city) {
     const data = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${key}&lang=pt_br&units=metric`).then(response => response.json())
     setHtml(data)
 }
 
+//This function get the input search and execute
 
 function getInputData() {
    const city = document.querySelector('.search').value
